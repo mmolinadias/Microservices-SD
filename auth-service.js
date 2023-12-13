dba140c907f0785a16ec19253d8a8c9ec31bc51e
unknown <ra120118@uem.br>
2023-12-13 16:12:09 -0300

    finalizado

--- auth-service.js
+++ auth-service.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const axios = require('axios'); // Adicionando o módulo axios para fazer requisições HTTP
-
+const axios = require('axios');
 const app = express();
 const port = 3000;
 const secretKey = 'seuSegredoSuperSecreto';
-const passwordServiceUrl = 'http://localhost:3001'; // URL do microsserviço de alteração de senha
+const passwordServiceUrl = 'http://localhost:3001';
 
 app.use(bodyParser.json());
 
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body;
-  console.log(`Usuário: ${username}`);
-  // Lógica para registrar o usuário no banco de dados (simulado por um log)
+  
   console.log(`Usuário registrado: ${username}`);
   
-  // Gera um token JWT
   const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
   
-  // Faz uma chamada HTTP para o microsserviço de alteração de senha para realizar ações adicionais
   try {
     await axios.post(`${passwordServiceUrl}/register-user`, { username, token });
     res.json({ message: 'Usuário registrado com sucesso', token });
--- password-service.js
+++ password-service.js
@@ -7,31 +7,40 @@ const port = 3001;
 const secretKey = 'seuSegredoSuperSecreto';
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/register-user', (req, res) => {
   const { username, token } = req.body;
 
-  // Verifica a autenticidade do token recebido do microsserviço de registro
   jwt.verify(token, secretKey, (err, user) => {
     if (err) {
       return res.status(403).json({ message: 'Token inválido' });
     }
 
-    // Lógica para armazenar o usuário ou realizar ações adicionais
     console.log(`Usuário registrado no microsserviço de alteração de senha: ${user.username}`);
     
     res.json({ message: 'Usuário registrado com sucesso no microsserviço de alteração de senha' });
   });
 });
 
-app.put('/change-password', authenticateToken, (req, res) => {
+app.put('/change-password', authenticateToken, authenticateUser, (req, res) => {
   const { username, newPassword } = req.body;
   
-  // Lógica para alterar a senha do usuário no banco de dados (simulado por um log)
   console.log(`Senha alterada para o usuário ${username}: ${newPassword}`);
   
   res.json({ message: 'Senha alterada com sucesso' });
 });
+
+function authenticateUser(req, res, next) {
+  const { username, password } = req.body;
+
+  if (username !== 'usuario') {
+    console.log(`Usuário inválido: ${username}`);
+    return res.status(401).json({ message: 'Usuário inválido' });
+  }
+  req.user = { username };
+  next();
+}
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
